00d94307c2dffb41b4a61b3a70e3cac9a4742d6f
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2023-06-10 12:52:03 +0530

    changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+type Cab = {
+    regNo: string
+    model: string
+    color: string
+};
 
 function App() {
-    const [count, setCount] = useState(0)
+    const [cabs, setCabs] = useState<Cab[]>([])
+
+    useEffect(() => {
+        const fetchCabs = async () => {
+            const { data } = await axios.get<Cab[]>('http://localhost:8000/')
+            setCabs(data)
+        }
+        fetchCabs()
+    }, [])
 
     return (
         <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+            <h1>Counter</h1>
         </>
     )
 }
